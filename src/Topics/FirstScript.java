1fa9faf4c0b7e34e9a080bca5a018a56529abe11
shree081297 <srikanthkurella123@gmail.com>
2024-06-25 10:58:32 +0530

    First Commit

---
+++ src/Topics/FirstScript.java
@@ -0,0 +1,11 @@
+package Topics;
+
+public class FirstScript {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Levitica");
+		System.out.println("123");
+	}
+
+}
---
+++ src/Topics/varaibles.java
@@ -0,0 +1,10 @@
+package Topics;
+
+public class varaibles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
